69b75ec039c655ced4bd66f9c61529284d85b2ae
Jakob Richter <code@jakob-r.de>
2019-06-05 11:28:28 +0200

    no need of kableExtra atm

--- tic.R
+++ tic.R
@@ -1,9 +1,5 @@
 do_package_checks(error_on = "error")
 
-# for md table in README
-get_stage("install") %>%
-  add_step(step_install_cran("kableExtra"))
-
 if (ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown(orphan = TRUE)
 }
