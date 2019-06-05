08c55add52c6680300952d9cfd0b8598c89c943f
pat-s <patrick.schratz@gmail.com>
2019-06-05 11:24:49 +0200

    require kableExtra

--- tic.R
+++ tic.R
@@ -1,5 +1,9 @@
 do_package_checks(error_on = "error")
 
+# for md table in README
+get_stage("install") %>%
+  add_step(step_install_cran("kableExtra"))
+
 if (ci_has_env("BUILD_PKGDOWN")) {
   do_pkgdown(orphan = TRUE)
 }
