6057c0838e213249096d0b185c931d57c64d8415
pat-s <patrick.schratz@gmail.com>
2019-06-17 18:37:44 +0200

    add a test for .$combine()

--- tests/testthat/test_Filter.R
+++ tests/testthat/test_Filter.R
@@ -7,3 +7,25 @@ test_that("Errors for unsupported features", {
   filter = FilterLinearCorrelation$new()
   expect_error(filter$calculate(task))
 })
+
+test_that("fr$combine()", {
+
+  task = mlr3::mlr_tasks$get("iris")
+
+  # create filters
+  fr_new = FilterMIM$new()
+  fr_new$calculate(task)
+  fr_new2 = FilterVariance$new()
+  fr_new2$calculate(task)
+
+  fr_comb = fr_new$combine(fr_new2)
+
+  expect_filter_result(fr_new)
+  expect_filter_result(fr_new2)
+  expect_filter_result(fr_comb)
+
+  expect_data_table(fr_new$scores, nrow = 4)
+  expect_data_table(fr_new2$scores, nrow = 4)
+  expect_data_table(fr_comb$scores, nrow = 8)
+
+})
