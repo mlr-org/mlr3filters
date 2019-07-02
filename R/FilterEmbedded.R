f29012083e7af15457f6948d5784bd6d053a23f1
pat-s <patrick.schratz@gmail.com>
2019-07-02 15:24:06 +0200

    FilterEmbedded: add ParamSet

--- R/FilterEmbedded.R
+++ R/FilterEmbedded.R
@@ -22,14 +22,20 @@ FilterEmbedded = R6Class("FilterEmbedded", inherit = Filter,
   public = list(
     learner = mlr3::mlr_learners$get("classif.rpart"),
     initialize = function(id = "embedded", learner = self$learner) {
-      self$learner = assert_learner(learner, properties = "importance")
+      self$learner = assert_learner(learner, properties = "importance",
+        param_vals = list())
 
       super$initialize(
         id = id,
         packages = learner$packages,
         feature_types = learner$feature_types,
         task_type = learner$task_type,
-        param_set = learner$param_set$clone(deep = TRUE)
+        param_set = ParamSet$new(list(
+          ParamInt$new("abs", lower = 1, tags = "generic"),
+          ParamDbl$new("perc", lower = 0, upper = 1, tags = "generic"),
+          ParamDbl$new("thresh", tags = "generic")
+        )),
+        param_vals = param_vals
       )
     }
   ),
