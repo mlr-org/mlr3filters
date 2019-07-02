0f711fd4edf574f4ed4c5be60509752768822021
pat-s <patrick.schratz@gmail.com>
2019-07-02 15:58:52 +0200

    NJMIM: add ParamSet

--- R/FilterNJMIM.R
+++ R/FilterNJMIM.R
@@ -4,9 +4,13 @@
 #' @format [R6::R6Class] inheriting from [Filter].
 #' @include Filter.R
 #'
-#' @description
-#' Minimal normalised joint mutual information maximisation filter.
-#' Calls [praznik::NJMIM()].
+#' @description Minimal normalised joint mutual information maximisation filter.
+#'   Calls [praznik::NJMIM()].
+#'
+#' @details This filter supports partial scoring via hyperparameter `k`. To use
+#'   it, set `k` during construction via `param_vals.` By default all filter
+#'   scores are calculated and the default of `k = 3` in the ParamSet does not
+#'   apply.
 #'
 #' @family Filter
 #' @export
@@ -17,7 +21,7 @@
 #' as.data.table(filter)[1:3]
 FilterNJMIM = R6Class("FilterNJMIM", inherit = Filter,
   public = list(
-    initialize = function(id = "njmim") {
+    initialize = function(id = "njmim", param_vals = list()) {
       super$initialize(
         id = id,
         packages = "praznik",
@@ -29,9 +33,32 @@ FilterNJMIM = R6Class("FilterNJMIM", inherit = Filter,
 
   private = list(
     .calculate = function(task, n = NULL) {
+
+
+      # setting params
+      k = self$param_set$values$k
+      threads = self$param_set$values$threads
+
+      if (is.null(k)) {
+        # by default we calculate all scores
+        # partial scoring need to be specifically requested by setting k during construction
+        k = length(task$feature_names)
+      }
+      if (is.null(threads)) {
+        threads = self$param_set$default$threads
+      }
+
+      # n overwrites any param_vals
+      if (!is.null(n)) {
+        if (!is.null(k)) {
+          warningf("Overwriting hyperparameter 'k' with the value given in `$filter_*().")
+        }
+        k = n
+      }
+
       X = task$data(cols = task$feature_names)
       Y = task$truth()
-      praznik::NJMIM(X = X, Y = Y, k = ncol(X))$score
+      praznik::NJMIM(X = X, Y = Y, k = k, threads = threads)$score
     }
   )
 )
