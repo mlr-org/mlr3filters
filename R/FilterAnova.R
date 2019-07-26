63ba7b64ee973d44fb4af79e09f3965154663dff
Michel Lang <michellang@gmail.com>
2019-07-26 21:48:37 +0200

    add anova filter

---
+++ R/FilterAnova.R
@@ -0,0 +1,53 @@
+#' @title ANOVA F-Test Filter
+#'
+#' @aliases mlr_filters_anova
+#' @format [R6::R6Class] inheriting from [Filter].
+#' @include Filter.R
+#'
+#' @description
+#' ANOVA F-Test filter calling [stats::aov()].
+#' Note that this is equivalent to a t-test for binary classification.
+#'
+#' The filter value is `-log10(p)` where `p` is the p-value.
+#' This transformation is necessary to ensure numerical stability for very small p-values.
+#'
+#' @family Filter
+#' @export
+#' @examples
+#' task = mlr3::mlr_tasks$get("iris")
+#' filter = FilterAnova$new()
+#' filter$calculate(task)
+#' as.data.table(filter)
+#'
+#' # transform to p-value
+#' 10^(-filter$scores)
+FilterAnova = R6Class("FilterAnova", inherit = Filter,
+  public = list(
+    initialize = function(id = "anova") {
+      super$initialize(
+        id = id,
+        packages = "stats",
+        feature_types = c("integer", "numeric"),
+        task_type = "classif"
+      )
+    }
+  ),
+
+  private = list(
+    .calculate = function(task, nfeat) {
+      data = task$data()
+      target = task$target_names
+      features = task$feature_names
+      p = map_dbl(features, function(fn) {
+        f = formulate(fn, target)
+        summary(aov(f, data = data))[[1L]][1L, "Pr(>F)"]
+      })
+
+      set_names(-log10(p), features)
+    }
+  )
+)
+
+#' @include mlr_filters.R
+mlr_filters$add("anova", FilterAnova)
+
