a39daadf721595b6da81b0df5b22800ff13ae2d4
pat-s <patrick.schratz@gmail.com>
2020-02-19 23:20:20 +0100

    switch from Travis to GH Actions

--- tic.R
+++ tic.R
@@ -4,9 +4,7 @@ do_package_checks(error_on = "warning")
 get_stage("install") %>%
   add_step(step_install_cran("ranger"))
 
-if (ci_on_travis()) {
-  do_pkgdown()
-}
+do_pkgdown()
 
 get_stage("after_success") %>%
   add_code_step(system("curl -s https://raw.githubusercontent.com/mlr-org/mlr3orga/master/trigger-mlr3book.sh | bash"))
