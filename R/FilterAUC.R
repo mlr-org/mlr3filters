2220da68a75e964b9f23c27991604969b8907936
pat-s <patrick.schratz@gmail.com>
2020-02-24 08:02:44 +0100

    fix coverage, document

--- R/FilterAUC.R
+++ R/FilterAUC.R
@@ -58,7 +58,7 @@ auc = function(truth, prob) {
   n_pos = sum(truth)
   n_neg = length(truth) - n_pos
   if (n_pos == 0L || n_neg == 0L) {
-    return(0.5)
+    return(0.5) # nocov
   }
   r = rank(prob, ties.method = "average")
   (sum(r[truth]) - n_pos * (n_pos + 1L) / 2L) / (n_pos * n_neg)
