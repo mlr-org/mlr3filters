1a2f26f481ee87ff7c2707d4e70e83fc310162b5
pat-s <patrick.schratz@gmail.com>
2020-04-16 11:24:58 +0200

    only deploy when BUILD_PKGDOWN=true is set

--- tic.R
+++ tic.R
@@ -5,7 +5,11 @@ get_stage("install") %>%
   add_step(step_install_cran("ranger")) %>%
   add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
 
-do_pkgdown()
+if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+  # creates pkgdown site and pushes to gh-pages branch
+  # only for the runner with the "BUILD_PKGDOWN" env var set
+  do_pkgdown()
+}
 
 if (Sys.info()["sysname"] == "Linux") {
   do_readme_rmd()
