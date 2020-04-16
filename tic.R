4b4da369c79fbae2a2465fcd1678c59bdf3a6c06
Patrick Schratz <patrick.schratz@gmail.com>
2020-04-16 13:18:40 +0200

    use mlr3pkgdowntemplate

--- tic.R
+++ tic.R
@@ -8,7 +8,9 @@ get_stage("install") %>%
 if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
-  do_pkgdown(orphan = TRUE)
+  get_stage("install") %>%
+    add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
+  do_pkgdown()
 }
 
 if (Sys.info()["sysname"] == "Linux" && ci_get_branch() == "master") {
