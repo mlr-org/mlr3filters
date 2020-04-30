b22c913429371ea9caa97ee97134511888f5b7d1
pat-s <patrick.schratz@gmail.com>
2020-04-30 17:36:08 +0200

    dont update README via CI

--- tic.R
+++ tic.R
@@ -1,22 +1,16 @@
 do_package_checks(error_on = "warning")
 
-# install ranger for README
 get_stage("install") %>%
+  # install ranger for README
   add_step(step_install_cran("ranger")) %>%
   add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
 
-if (ci_on_ghactions() && ci_has_env("BUILD_PKGDOWN")) {
+if (ci_on_ghactions()) {
   # creates pkgdown site and pushes to gh-pages branch
   # only for the runner with the "BUILD_PKGDOWN" env var set
   get_stage("install") %>%
     add_step(step_install_github("mlr-org/mlr3pkgdowntemplate"))
   do_pkgdown()
-}
-
-if (Sys.info()["sysname"] == "Linux" && ci_get_branch() == "master") {
-  get_stage("deploy") %>%
-    add_step(step_install_github("mlr-org/mlr3filters"))
-  do_readme_rmd()
 }
 
 get_stage("after_success") %>%
