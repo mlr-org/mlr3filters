7d915f243595014338b70b6c71e91481469efef5
Michel Lang <michellang@gmail.com>
2021-10-06 07:07:16 +0200

    add missing file

---
+++ R/bibentries.R
@@ -0,0 +1,26 @@
+#' @importFrom utils bibentry
+bibentries     = c(
+  bommert_2020 = bibentry("article",
+    doi        = "10.1016/j.csda.2019.106839",
+    year       = "2020",
+    month      = "3",
+    publisher  = "Elsevier {BV}",
+    volume     = "143",
+    pages      = "106839",
+    author     = "Andrea Bommert and Xudong Sun and Bernd Bischl and J\u00f6rg Rahnenf\u00fchrer and Michel Lang",
+    title      = "Benchmark for filter methods for feature selection in high-dimensional classification data",
+    journal    = "Computational Statistics {\\&} Data Analysis"
+  ),
+
+ kursa_2021    = bibentry("article",
+    doi        = "10.1016/j.softx.2021.100819",
+    year       = "2021",
+    month      = "12",
+    publisher  = "Elsevier {BV}",
+    volume     = "16",
+    pages      = "100819",
+    author     = "Miron B. Kursa",
+    title      = "Praznik: High performance information-based feature selection",
+    journal    = "{SoftwareX}"
+  )
+)
