293c31b7f5ac578aca92b54aee378b1a25aa0abe
Lona <lona.koers@gmail.com>
2024-12-20 17:18:15 +0100

    Remove sprintf and add cli inline markdown.

--- R/Filter.R
+++ R/Filter.R
@@ -120,16 +120,18 @@ Filter = R6Class("Filter",
     #' @description
     #' Printer for Filter class
     print = function() {
-      cli_h1(sprintf("%s %s%s", class(self)[1L], self$id, if (is.na(self$label)) "" else paste0(": ", self$label)))
-      cli_li(sprintf("Task Types: %s", paste(self$task_types, collapse = ", ")))
-      cli_li(sprintf("Properties: %s", if (length(self$properties)) paste(self$properties, collapse = ", ") else "-"))
-      cli_li(sprintf("Task Properties: %s", paste(self$task_properties, collapse = ", ")))
-      cli_li(sprintf("Packages: %s", paste(self$packages, collapse = ", ")))
-      cli_li(sprintf("Feature types: %s", paste(self$feature_types, collapse = ", ")))
+      msg_h = if (is.na(self$label)) "" else paste0(": ", self$label)
+      cli_h1("{.cls {class(self)[1L]}} {self$id}{msg_h}")
+      cli_li("Task Types: {self$task_types}")
+      properties = if (length(self$properties)) paste(self$properties, collapse = ", ") else "-"
+      cli_li("Properties: {properties}")
+      cli_li("Task Properties: {self$task_properties}")
+      cli_li("Packages: {.pkg {self$packages}}")
+      cli_li("Feature types: {self$feature_types}")
       if (length(self$scores)) {
         print(as.data.table(self),
-          nrows = 10L, topn = 5L, class = FALSE,
-          row.names = TRUE, print.keys = FALSE)
+              nrows = 10L, topn = 5L, class = FALSE,
+              row.names = TRUE, print.keys = FALSE)
       }
     },
 
